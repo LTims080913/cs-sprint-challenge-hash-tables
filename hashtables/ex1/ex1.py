07d96221463cdf35dcb55d0e699dc75293dfa195
Latosha Tims <ltims080913@hotmail.com>
2020-07-10 08:35:24 -0700

    finished ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
 
+    box = {}
+    for x in range(len(weights)):
+        box[weights[x]] = x
+    for x in range(len(weights)):
+        dif = limit-weights[x]
+        if dif in box:
+            return (max(x, box[dif]), min(x, box[dif]))
     return None
